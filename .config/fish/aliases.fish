6c36f5e55093572b1c3bc5e52c1d6a09b920d472
GabhenDM <henriques.gabriel@outlook.com>
2025-03-07 21:25:22 +0100

    fish changes

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,5 +1,11 @@
-alias k 'kubectl'
+
 alias ll 'ls -lha'
 alias l 'ls'
 alias g git
 alias v nvim
+
+
+# Kubectl
+alias k 'kubectl'
+alias kens 'kubectl ns'
+alias ktx 'kubectl ctx'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
+. ~/.config/fish/aliases.fish
+
 # ASDF configuration code
 if test -z $ASDF_DATA_DIR
     set _asdf_shims "$HOME/.asdf/shims"
@@ -9,6 +11,10 @@ end
 # potentially changes the order of items in PATH
 if not contains $_asdf_shims $PATH
     set -gx --prepend PATH $_asdf_shims
+end
+
+if not contains "$HOME/.krew/bin" $PATH
+    set -gx --prepend PATH "$HOME/.krew/bin"
 end
 set --erase _asdf_shims
 
